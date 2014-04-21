334c889a2c71d1b2a0dedfea49b4bfbe059dbce8
Bul Ikana <bul.ikana@gmail.com>
2014-04-21 04:41:33 -0500

    ADDD SIMPLE BUT KAWAII HEADR

--- template/index.php
+++ template/index.php
@@ -11,37 +11,25 @@
         <link rel="stylesheet" href="<?php echo($template_dir_url); ?>style.css">
         <link rel="stylesheet" href="<?php echo($template_dir_url); ?>subdiv.css">
         <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
         
         <?php get_header(); ?>
     </head>
 
     <body>
-        <?php if($is_home) { ?>
-        <article>
-            <div class="row">
-                <div class="one-quarter meta">
-                    <div class="thumbnail">
-                        <img src="<?php echo get_twitter_profile_img($blog_twitter); ?>" alt="profile" />
-                    </div>
-        
-                    <ul>
-                        <li><?php echo($blog_title); ?></li>
-                        <li><a href="mailto:<?php echo($blog_email); ?>?subject=Hello"><?php echo($blog_email); ?></a></li>
-                        <li><a href="http://twitter.com/<?php echo($blog_twitter); ?>">&#64;<?php echo($blog_twitter); ?></a></li>
-                        <li></li>
-                    </ul>
-                </div>
-        
-                <div class="three-quarters post">
-                    <h2><?php echo($intro_title); ?></h2>
-        
-                    <p><?php echo($intro_text); ?></p>
-        
-                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-                </div>
-            </div>
-        </article>
-        <?php } ?>
+	
+	
+		<div class="header">
+			<div class="row">
+				<div class="one-quarter">
+					<a href="<?php echo($blog_url);?>"><img src="./cache/<?php echo($blog_twitter);?>.jpg" id="head-img"></a>
+				</div>
+				<div class="three-quarters" id="head-txt">
+					<h1><?php echo($blog_title);?></h1>
+					<p><?php echo($meta_description);?></p>
+				</div>
+			</div>
+		</div>
         
         <?php echo($content); ?>
         
