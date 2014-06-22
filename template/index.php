458bb99a4729a62511ef980129e3836a906dd05f
Bul Ikana <bul.ikana@gmail.com>
2014-06-22 16:01:34 -0500

    LATERAL PANE WUZ OVERRIDEN

--- template/index.php
+++ template/index.php
@@ -34,5 +34,31 @@
         <?php echo($content); ?>
         
         <?php get_footer(); ?>
+        <script type="text/javascript">
+        	var panelContent = 
+        	'<div class="dp-panel">' + 
+        		'<div class="dp-row profile">' + 
+        			'<div class="dp-content">' + 
+        				'<span class="title"><?php echo $intro_title?></span>' + 
+        				'<a class="dp-button dp-button-dark dp-close dp-icon-close" href="#dp-dropplets"></a>' + 
+            		'</div>' + 
+        		'</div>' + 
+        		'<img src="./cache/<?php echo $blog_twitter?>.jpg" id="lateral-img">' + 
+        		'<p id="description"><?php echo $intro_text?></p>' + 
+        		'<div class="dp-row profile">' + 
+        			'<div class="dp-content">' + 
+        				'<span class="title">Find me on Twitter</span>' + 
+        				'<a class="dp-button dp-button-dark dp-close dp-icon-twitter" href="http://twitter.com/' + '<?php echo $blog_twitter?>' +'"></a>' + 
+            		'</div>' + 
+        		'</div>' + 
+        		'<div class="dp-row drop-row">' + 
+        			'<div class="dp-content">' + 
+        				'<span class="title">Blawg made with Dropplets!</span>' + 
+        				'<a class="dp-button dp-button-dark dp-close dp-icon-drop" href="http://dropplets.com"></a>' + 
+            		'</div>' + 
+        		'</div>' + 
+			'</div>';
+			document.getElementById("dp-dropplets").innerHTML = panelContent;
+		</script>
     </body>
 </html>
